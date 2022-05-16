39026da1c965267dde76b6a5822d464d4c10cbd5
Ivan <bykoviv78@gmail.com>
2022-05-16 22:56:25 +0700

    7.10

--- feedback/views.py
+++ feedback/views.py
@@ -17,6 +17,17 @@ def index(request):
         form = FeedbackForm()
     return render(request, 'feedback/feedback.html', context={'form': form})
 
+def update_feedback(request, id_feedback):
+    feed = Feedback.objects.get(id=id_feedback)
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST, instance=feed)
+        if form.is_valid():
+            form.cleaned_data
+            form.save()
+            return HttpResponseRedirect(f'/{id_feedback}')
+    else:
+        form = FeedbackForm(instance=feed)
+    return render(request, 'feedback/feedback.html', context={'form': form})
 
 def done(request):
     return render(request, 'feedback/done.html')
