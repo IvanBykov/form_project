bc8e2f1057ab8f16f2e9ba12757d82c20e584157
Ivan <bykoviv78@gmail.com>
2022-05-16 17:48:45 +0700

    7.9

--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import FeedbackForm
+from .models import Feedback
+
 
 # Create your views here.
 
@@ -8,9 +10,13 @@ def index(request):
     if request.method == 'POST':
         form = FeedbackForm(request.POST)
         if form.is_valid():
+            form.cleaned_data
+            form.save()
             return HttpResponseRedirect('/done')
-    form = FeedbackForm()
+    else:
+        form = FeedbackForm()
     return render(request, 'feedback/feedback.html', context={'form': form})
+
 
 def done(request):
     return render(request, 'feedback/done.html')
