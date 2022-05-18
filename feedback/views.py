f5415b20ca221b6736fc7f8248241ebdd963c653
Ivan <bykoviv78@gmail.com>
2022-05-18 16:58:12 +0700

    8.3

--- feedback/views.py
+++ feedback/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from .forms import FeedbackForm
 from .models import Feedback
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from django.views import View
 
@@ -42,15 +43,33 @@ class DoneView(TemplateView):
         context['date'] = '23.04.2022'
         return context
 
+class DetailFeedBack(TemplateView):
+    template_name = 'feedback/detail_feedback.html'
 
-class ListFeedBack(TemplateView):
-    template_name = 'feedback/list_feedback.html'
-
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, id_feedback, **kwargs):
         context = super().get_context_data(**kwargs)
-        list_feed = Feedback.objects.all()
-        context['list_feed'] = list_feed
+        feed = Feedback.objects.get(id=id_feedback)
+        context['feed'] = feed
         return context
+
+# class ListFeedBack(TemplateView):
+#    template_name = 'feedback/list_feedback.html'
+
+#    def get_context_data(self, **kwargs):
+#        context = super().get_context_data(**kwargs)
+#        list_feed = Feedback.objects.all()
+#        context['list_feed'] = list_feed
+#        return context
+
+class ListFeedBack(ListView):
+    template_name = 'feedback/list_feedback.html'
+    model = Feedback
+    context_object_name = 'feedbacks'
+
+    def get_queryset(self):
+        #queryset = super().get_queryset()
+        #filter_qs = queryset.filter(rating__gt=3)
+        return super().get_queryset().filter(rating__gt=2)
 
 
 # Create your views here.
